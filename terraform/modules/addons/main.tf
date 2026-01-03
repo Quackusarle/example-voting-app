a29be2dd51c4019364f1544abd9a07b8fd2a7502
quack <tvhoangg05@gmail.com>
2026-01-04 00:35:07 +0700

    Add: eks terraform

---
+++ terraform/modules/addons/main.tf
@@ -0,0 +1,83 @@
+resource "aws_eks_addon" "ebs_csi" {
+  cluster_name = var.cluster_name
+  addon_name   = "aws-ebs-csi-driver"
+}
+
+resource "kubernetes_storage_class" "ebs_sc" {
+  metadata { name = "ebs-sc" }
+  storage_provisioner = "ebs.csi.aws.com"
+  reclaim_policy      = "Retain"
+  volume_binding_mode = "WaitForFirstConsumer"
+  parameters          = { type = "gp3" }
+}
+
+resource "helm_release" "alb_controller" {
+  name       = "aws-load-balancer-controller"
+  repository = "https://aws.github.io/eks-charts"
+  chart      = "aws-load-balancer-controller"
+  namespace  = "kube-system"
+  version    = "1.9.1"
+
+  set {
+    name  = "clusterName"
+    value = var.cluster_name
+  }
+
+  set {
+    name  = "serviceAccount.create"
+    value = "true"
+  }
+
+  set {
+    name  = "serviceAccount.name"
+    value = "aws-load-balancer-controller"
+  }
+
+  
+  set {
+    name  = "vpcId"
+    value = var.vpc_id
+  }
+}
+
+resource "helm_release" "metrics_server" {
+  name       = "metrics-server"
+  repository = "https://kubernetes-sigs.github.io/metrics-server/"
+  chart      = "metrics-server"
+  namespace  = "kube-system"
+  version    = "3.12.2"
+}
+
+resource "helm_release" "argocd" {
+  name             = "argocd"
+  repository       = "https://argoproj.github.io/argo-helm"
+  chart            = "argo-cd"
+  namespace        = "argocd"
+  create_namespace = true
+  version          = "7.7.1"
+
+  values = [
+    yamlencode({
+      configs = {
+        params = {
+          "server.insecure" = true
+        }
+      }
+      server = {
+        ingress = {
+          enabled          = true
+          ingressClassName = "alb"
+          annotations = {
+            "alb.ingress.kubernetes.io/scheme"           = "internet-facing"
+            "alb.ingress.kubernetes.io/target-type"      = "ip"
+            "alb.ingress.kubernetes.io/backend-protocol" = "HTTP"
+            "alb.ingress.kubernetes.io/listen-ports"     = jsonencode([{HTTP=80}])
+          }
+          hosts = [""]
+        }
+      }
+    })
+  ]
+  
+  depends_on = [helm_release.alb_controller]
+}
---
+++ terraform/modules/eks/main.tf
@@ -0,0 +1,40 @@
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "19.21.0"
+
+  cluster_name    = var.cluster_name
+  cluster_version = var.cluster_version
+  vpc_id     = var.vpc_id
+  subnet_ids = var.subnet_ids
+  cluster_endpoint_public_access = true
+  create_iam_role = false
+  iam_role_arn    = var.iam_role_arn
+  enable_irsa = false
+  manage_aws_auth_configmap = true
+  aws_auth_roles = [
+    {
+      rolearn  = var.iam_role_arn
+      username = "admin"
+      groups   = ["system:masters"]
+    }
+  ]
+  create_kms_key              = false
+  create_cloudwatch_log_group = false
+  cluster_encryption_config   = {}
+  eks_managed_node_groups = {
+    main = {
+      min_size     = var.node_min_size
+      max_size     = var.node_max_size
+      desired_size = var.node_desired_size
+      instance_types = var.node_instance_types
+      create_iam_role = false
+      iam_role_arn    = var.iam_role_arn
+    }
+  }
+
+  cluster_addons = {
+    coredns    = { most_recent = true }
+    kube-proxy = { most_recent = true }
+    vpc-cni    = { most_recent = true }
+  }
+}
